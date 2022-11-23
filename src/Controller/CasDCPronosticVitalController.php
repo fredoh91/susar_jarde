e13278c0842625c9f6e71a3b5b74b2d28cec6129
fredoh91 <fredoh@gmail.com>
2022-11-23 14:18:50 +0100

    ok pour connexion PEMBA

--- src/Controller/CasDCPronosticVitalController.php
+++ src/Controller/CasDCPronosticVitalController.php
@@ -31,4 +31,38 @@ class CasDCPronosticVitalController extends AbstractController
             'em' => $em,
         ]);
     }
+    #[Route('/test_pemba_2', name: 'test_pemba_2')]
+    public function test_pemba_2(ManagerRegistry $doctrine): Response
+    {
+        
+        $this->em = $doctrine->getManager('pemba');
+        // $sql = "UPDATE grille_occ_em_v2 SET vmin=$this->vmin_3, vmax=$this->vmax_3 WHERE cat_idx=3;";
+
+
+
+        $sql = "SELECT DISTINCT "
+             .  "mv.id, mv.caseid, mv.specificcaseid, mv.DLPVersion, " 
+             .  "st.studytitle, st.sponsorstudynumb, "
+             .  "sr.studyname num_eudract, sr.studyregistrationcountry pays_etude "
+             .  "FROM master_versions mv "
+             .  "LEFT JOIN bi_study st ON mv.id = st.master_id "
+             .  "LEFT JOIN bi_study_registration sr ON mv.id = sr.master_id "
+             .  "WHERE 1 = 1 "
+             .  "AND specificcaseid LIKE 'EC%' "
+             .  "AND mv.CreationDate = '2022-09-30' "
+             .  "AND mv.Deleted = 0;";
+
+
+        
+             $stmt = $this->em->getConnection()->prepare($sql);
+             $stmt_2= $stmt->execute()->fetchAll(); 
+            //  $stmt_2= $stmt->execute(); 
+
+
+
+        return $this->render('cas_dc_pronostic_vital/index.html.twig', [
+            'controller_name' => 'CasDCPronosticVitalController',
+            'em' => $stmt_2,
+        ]);
+    }
 }
